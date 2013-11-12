2058850547f89a993620851b7a9eee9524b06e5a
ebyjane <eby.jane@gmail.com>
2013-11-12 14:26:41 +0530

    New project dweling
    New project dweling

---
+++ protected/views/site/pages/advice.php
@@ -0,0 +1,12 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - Advice';
+$this->breadcrumbs=array(
+	'Advice',
+);
+?>
+<h1>Advice</h1>
+
+<p>This is a "static" page. You may change the content of this page
+by updating the file <code><?php echo __FILE__; ?></code>.</p>
